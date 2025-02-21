a994f249c6f0e2c63f98cb22c67a479ccaf1a8a3
Kowsalya-SE <155236764+Kowsalya-SE@users.noreply.github.com>
2025-02-21 18:13:13 +0000

    login added changes

--- routes/auth.router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller.js');
-
-router.post('/auth/google',authController.login)  
-module.exports = router;
